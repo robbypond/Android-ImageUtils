1f4a6c2f93696b731d7761b198390954c0acab0e
Robby Pond <robert.w.pond@gmail.com>
2012-08-30 19:37:55 -0400

    Creation

---
+++ src/com/rpond/imageutils/UIUtils.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rpond.imageutils;
+
+import android.os.Build;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * An assortment of UI helpers.
+ */
+public class UIUtils {
+
+    public static ImageFetcher getImageFetcher(final FragmentActivity activity) {
+        // The ImageFetcher takes care of loading remote images into our ImageView
+        ImageFetcher fetcher = new ImageFetcher(activity);
+        fetcher.addImageCache(activity);
+        return fetcher;
+    }
+
+    public static boolean hasFroyo() {
+        // Can use static final constants like FROYO, declared in later versions
+        // of the OS since they are inlined at compile time. This is guaranteed behavior.
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.FROYO;
+    }
+
+    public static boolean hasGingerbread() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD;
+    }
+
+    public static boolean hasHoneycomb() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
+    }
+
+    public static boolean hasHoneycombMR1() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1;
+    }
+}
